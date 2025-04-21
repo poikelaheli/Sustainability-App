59901db5ad0cec9bb29bfaea3a5673c355fb3ed6
poikelaheli <151742475+poikelaheli@users.noreply.github.com>
2025-04-21 20:06:28 +0300

    add login screens and logic

---
+++ app/src/main/java/com/example/sustainabilityapp/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.sustainabilityapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sustainabilityapp.ui.theme.SustainabilityAppTheme
+
+class MainActivity : ComponentActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_registration)
+        /*setContent {
+            SustainabilityAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }*/
+    }
+
+    override fun onClick(v: View?) {
+        val loginButtons: LinearLayout = this.findViewById<LinearLayout>(R.id.loginButtons)
+        val loginForm: LinearLayout = this.findViewById<LinearLayout>(R.id.loginForm)
+        val registrationForm: LinearLayout = this.findViewById<LinearLayout>(R.id.registrationForm)
+        val navigationButtons: LinearLayout = this.findViewById<LinearLayout>(R.id.navigationButtons)
+
+        when (v?.id) {
+            R.id.openLogin -> {
+                loginButtons.visibility = View.GONE
+                loginForm.visibility = View.VISIBLE
+                registrationForm.visibility = View.GONE
+                navigationButtons.visibility = View.VISIBLE
+            }
+            R.id.openRegistration -> {
+                loginButtons.visibility = View.GONE
+                loginForm.visibility = View.GONE
+                registrationForm.visibility = View.VISIBLE
+                navigationButtons.visibility = View.VISIBLE
+            }
+            R.id.returnButton -> {
+                loginButtons.visibility = View.VISIBLE
+                loginForm.visibility = View.GONE
+                registrationForm.visibility = View.GONE
+                navigationButtons.visibility = View.INVISIBLE
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SustainabilityAppTheme {
+        Greeting("Android")
+    }
+}
