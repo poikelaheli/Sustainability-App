d028e612474dd21db7053a4aebdffb2e799760e1
poikelaheli <151742475+poikelaheli@users.noreply.github.com>
2025-05-02 22:10:32 +0300

    add logout logic

--- app/src/main/java/com/example/sustainabilityapp/HomeFragment.kt
+++ app/src/main/java/com/example/sustainabilityapp/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.example.sustainabilityapp
 
+import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment(R.layout.home) {
--- app/src/main/java/com/example/sustainabilityapp/MainActivity.kt
+++ app/src/main/java/com/example/sustainabilityapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sustainabilityapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
@@ -11,7 +12,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.sustainabilityapp.databinding.ActivityMainBinding
 import com.example.sustainabilityapp.ui.theme.SustainabilityAppTheme
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
@@ -31,36 +32,64 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }*/
     }
 
-    override fun onClick(v: View?) {
-        val loginButtons: LinearLayout = this.findViewById<LinearLayout>(R.id.loginButtons)
-        val loginForm: LinearLayout = this.findViewById<LinearLayout>(R.id.loginForm)
-        val registrationForm: LinearLayout = this.findViewById<LinearLayout>(R.id.registrationForm)
-        val navigationButtons: LinearLayout = this.findViewById<LinearLayout>(R.id.navigationButtons)
+    fun onClick(v: View?) {
 
         when (v?.id) {
             R.id.openLogin -> {
-                loginButtons.visibility = View.GONE
-                loginForm.visibility = View.VISIBLE
-                registrationForm.visibility = View.GONE
-                navigationButtons.visibility = View.VISIBLE
+                updateLayoutVisibility("openLogin")
             }
             R.id.openRegistration -> {
-                loginButtons.visibility = View.GONE
-                loginForm.visibility = View.GONE
-                registrationForm.visibility = View.VISIBLE
-                navigationButtons.visibility = View.VISIBLE
+                updateLayoutVisibility("openRegistration")
             }
             R.id.returnButton -> {
-                loginButtons.visibility = View.VISIBLE
-                loginForm.visibility = View.GONE
-                registrationForm.visibility = View.GONE
-                navigationButtons.visibility = View.INVISIBLE
+                updateLayoutVisibility("returnButton")
             }
             R.id.loginFormButton -> {
                 val fragmentManager = supportFragmentManager
                 val fragmentTransaction = fragmentManager.beginTransaction()
                 fragmentTransaction.replace(R.id.fragmentContainer, HomeFragment())
                 fragmentTransaction.commit()
+            }
+            R.id.regFormButton -> {
+                val fragmentManager = supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.fragmentContainer, HomeFragment())
+                fragmentTransaction.commit()
+            }
+            R.id.logoutButton -> {
+                val fragmentManager = supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.fragmentContainer, LoginRegistrationFragment())
+                fragmentTransaction.commit()
+            }
+        }
+    }
+
+    fun updateLayoutVisibility (buttonName: String) {
+        val loginButtons: LinearLayout = this.findViewById<LinearLayout>(R.id.loginButtons)
+        val loginForm: LinearLayout = this.findViewById<LinearLayout>(R.id.loginForm)
+        val registrationForm: LinearLayout = this.findViewById<LinearLayout>(R.id.registrationForm)
+        val navigationButtons: LinearLayout = this.findViewById<LinearLayout>(R.id.navigationButtons)
+        when (buttonName) {
+            "openLogin" -> {
+                loginButtons.visibility = View.GONE
+                loginForm.visibility = View.VISIBLE
+                registrationForm.visibility = View.GONE
+                navigationButtons.visibility = View.VISIBLE
+            }
+
+            "openRegistration" -> {
+                loginButtons.visibility = View.GONE
+                loginForm.visibility = View.GONE
+                registrationForm.visibility = View.VISIBLE
+                navigationButtons.visibility = View.VISIBLE
+            }
+
+            "returnButton" -> {
+                loginButtons.visibility = View.VISIBLE
+                loginForm.visibility = View.GONE
+                registrationForm.visibility = View.GONE
+                navigationButtons.visibility = View.INVISIBLE
             }
         }
     }
